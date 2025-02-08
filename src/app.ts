ff0e47a9b28b6808b91f3680fb3834400c8acc64
sayedulhoque44 <sayedulhoque3544@gmail.com>
2025-02-08 13:33:18 +0600

    fix main

--- src/app.ts
+++ src/app.ts
@@ -46,7 +46,7 @@ app.get("/test", async (req: Request, res: Response) => {
   try {
     await Promise.reject();
   } catch (error) {
-    res.send("Erro Occurs test");
+    res.send("Erro Occurs test main");
   }
 });
 
